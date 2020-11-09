95bb5121133986d0d29cdbb6df6b1b8504e2d289
riunosk <riunosk@outlook.com>
2020-11-09 09:34:49 +0800

    school

---
+++ Assets/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+    int spacebarPress = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "Start Function";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+            spacebarPress += 1;
+            print("Spacebar Pressed " + spacebarPress + " Times");
+            scoreText.GetComponent<Text>().text = "Spacebar " + spacebarPress + " Times";
+		}
+    }
+}
